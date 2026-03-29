03bc5d1e93caafa1d6fd77c43cb0e2d8b2d549b2
Josué Munganga <jmunganga039@gmail.com>
2026-03-29 15:22:54 +0100

    working

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -1,6 +1,13 @@
 import Flutter
 import UIKit
+import FirebaseAuth
 
 class SceneDelegate: FlutterSceneDelegate {
-
+  /// Phone Auth (reCAPTCHA) revient par URL ; avec UIScene il faut forwarder ici aussi.
+  override func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+    for context in URLContexts {
+      _ = Auth.auth().canHandle(context.url)
+    }
+    super.scene(scene, openURLContexts: URLContexts)
+  }
 }
